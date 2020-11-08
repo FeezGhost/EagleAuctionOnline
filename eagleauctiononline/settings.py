db708554d40c8cde509c83f579453d2a16611467
feezghost <feezoocrazy420@gmail.com>
2020-11-08 20:52:10 +0500

    Update settings.py

--- eagleauctiononline/settings.py
+++ eagleauctiononline/settings.py
@@ -147,7 +147,7 @@ AWS_STORAGE_BUCKET_NAME = 'eagleauctiongenius'
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+# STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 AWS_S3_HOST = "s3.us-east-2.amazonaws.com" 
 AWS_S3_REGION_NAME = "us-east-2"
 
